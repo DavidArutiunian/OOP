e1a904043a24e01f7aaee68c850bc2b1d22d2fe8
David Arutiunian <arutunjan666@mail.com>
2019-02-08 21:51:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OOP labs
+
+### Requirements
+
+* Java 11
+* Maven 3.6.0
---
+++ src/test/java/lab1/task1/MainTest.java
@@ -0,0 +1,11 @@
+package lab1.task1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}

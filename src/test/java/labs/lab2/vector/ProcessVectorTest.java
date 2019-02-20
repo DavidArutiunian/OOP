34f8c772023305b8fade328f45ef6316273c3705
David Arutiunian <arutunjan666@mail.com>
2019-02-20 23:23:17 +0300

    [2.1.1] Implement basic IO
    Cover with tests

---
+++ src/test/java/labs/lab2/vector/ProcessVectorTest.java
@@ -0,0 +1,86 @@
+package labs.lab2.vector;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class ProcessVectorTest {
+    @Test
+    public void testReadWorks() throws IOException {
+        setSystemInput("1.1 2.2 3.3");
+        final String[] arguments = readSystemInput();
+        assertEquals("1.1", arguments[0]);
+        assertEquals("2.2", arguments[1]);
+        assertEquals("3.3", arguments[2]);
+    }
+
+    @Test
+    public void testReadThrowsIOException() {
+        setSystemInput("\n");
+        final Scanner in = new Scanner(System.in);
+        assertThrows(IOException.class, () -> ProcessVector.read(in));
+    }
+
+    @Test
+    public void testParseWorks() throws IOException {
+        setSystemInput("1.1 2.2 3.3");
+        final String[] arguments = readSystemInput();
+        final List<Float> input = ProcessVector.parse(arguments);
+        assertEquals(Float.valueOf(1.1f), input.get(0));
+        assertEquals(Float.valueOf(2.2f), input.get(1));
+        assertEquals(Float.valueOf(3.3f), input.get(2));
+    }
+
+    @Test
+    public void testParseThrowsIOException() throws IOException {
+        setSystemInput("1.1 2.2a 3.3");
+        final String[] arguments = readSystemInput();
+        assertThrows(IOException.class, () -> ProcessVector.parse(arguments));
+    }
+
+    @Test
+    public void testPrintWorks() throws IOException {
+        final var mock = new OutputMock();
+        setSystemInput("1.1 2.2 3.3");
+        final String[] arguments = readSystemInput();
+        final List<Float> input = ProcessVector.parse(arguments);
+        ProcessVector.print(input);
+        assertEquals("1.100 2.200 3.300", mock.getInput().strip());
+        mock.destruct();
+    }
+
+    private void setSystemInput(final String input) {
+        final var in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
+    private String[] readSystemInput() throws IOException {
+        final Scanner in = new Scanner(System.in);
+        return ProcessVector.read(in);
+    }
+
+    private class OutputMock {
+        private final PrintStream original = System.out;
+        private final ByteArrayOutputStream mock = new ByteArrayOutputStream();
+
+        OutputMock() {
+            System.setOut(new PrintStream(mock));
+        }
+
+        String getInput() {
+            return mock.toString();
+        }
+
+        void destruct() {
+            System.setOut(original);
+        }
+    }
+}

66636831cc55f6ca893561e90a17a6641f5dd381
David Arutiunian <arutunjan666@mail.com>
2019-03-07 09:27:21 +0300

    Engine tests refactoring

--- src/test/java/labs/lab3/car/engine/EngineTest.java
+++ src/test/java/labs/lab3/car/engine/EngineTest.java
@@ -1,13 +1,19 @@
 package labs.lab3.car.engine;
 
 import labs.lab3.car.transmission.Gear;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class EngineTest {
-    private final Engine engine = new Engine();
+    private Engine engine = new Engine();
+
+    @Before
+    public void setUp() {
+        engine = new Engine();
+    }
 
     @Test
     public void testIsOffOnInit() {
@@ -16,58 +22,41 @@ public class EngineTest {
 
     @Test
     public void testIsOn() throws EngineIsOnException {
-        engine.on();
+        turnOnEngine();
         assertEquals(EngineState.ON, engine.getState());
     }
 
     @Test
-    public void testIsOff() throws EngineIsOffException {
-        try {
-            engine.on();
-        } catch (Exception e) {
-            // noop
-        }
+    public void testIsOff() throws EngineIsOffException, EngineIsOnException {
+        turnOnEngine();
         engine.off(Gear.NEUTRAL, 0);
         assertEquals(EngineState.OFF, engine.getState());
     }
 
     @Test
-    public void testThrowsIfAlreadyOn() {
-        try {
-            engine.on();
-        } catch (Exception e) {
-            // noop
-        }
+    public void testThrowsIfAlreadyOn() throws EngineIsOnException {
+        turnOnEngine();
         assertThrows(EngineIsOnException.class, engine::on);
     }
 
     @Test
     public void testThrowsIfAlreadyOff() {
-        try {
-            engine.off(Gear.NEUTRAL, 0);
-        } catch (Exception e) {
-            // noop
-        }
         assertThrows(EngineIsOffException.class, () -> engine.off(Gear.NEUTRAL, 0));
     }
 
     @Test
-    public void testThrowsIfOffOnSpeed() {
-        try {
-            engine.on();
-        } catch (Exception e) {
-            // noop
-        }
+    public void testThrowsIfOffOnSpeed() throws EngineIsOnException {
+        turnOnEngine();
         assertThrows(EngineIsOffException.class, () -> engine.off(Gear.NEUTRAL, 10));
     }
 
     @Test
-    public void testThrowsIfOffOnGear() {
-        try {
-            engine.on();
-        } catch (Exception e) {
-            // noop
-        }
+    public void testThrowsIfOffOnGear() throws EngineIsOnException {
+        turnOnEngine();
         assertThrows(EngineIsOffException.class, () -> engine.off(Gear.FIRST, 0));
+    }
+
+    private void turnOnEngine() throws EngineIsOnException {
+        engine.on();
     }
 }
